a0516f44ddec85957f9f42695315f08f59a955ad
George Farfan <georgef@gmail.com>
2023-01-25 14:54:37 +0100

    [eslint-solid] - rules added

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 module.exports = {
-    rules: {
-        "no-constant-condition": ["error", {"checkLoops": false}],
-        "no-loss-of-precision": "warn",
-        "no-regex-spaces": "warn",
-        "no-unreachable": "warn",
-        "constructor-super": "error",
-        "no-var": "error",
-        "no-console": ["error", { "allow": ["warn", "error", "log", "table"] }],
-        "max-lines": ["error", {"max": 100, "skipComments": true}],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "max-classes-per-file": ["error", 1],
-        "quotes": ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-unused-vars": [
-          "error", 
-          { "vars": "all", "args": "after-used", "ignoreRestSiblings": false 
-          }
-        ],
-        "max-len": ["error", {
-          "code": 100
-        }]
-    }
+  rules: {
+      "array-bracket-spacing": ["error", "always"],
+      "space-in-parens": ["error", "never"],
+      "no-constant-condition": ["error", {"checkLoops": false}],
+      "no-loss-of-precision": "warn",
+      "no-regex-spaces": "warn",
+      "no-unreachable": "warn",
+      "constructor-super": "error",
+      "no-var": "error",
+      "no-console": ["error", { "allow": ["warn", "error", "log", "table"] }],
+      "max-lines": ["error", {"max": 2000, "skipComments": true}],
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "always"],
+      "computed-property-spacing": ["error", "never"],
+      "comma-spacing": ["error", { "before": false, "after": true }],
+      "constructor-super": "error",
+      "linebreak-style": [
+        "error"
+    ],
+      "max-classes-per-file": ["error", 1],
+      "quotes": ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
+      "semi": [
+          "error",
+          "always"
+      ],
+      "no-unused-vars": [
+        "error",
+        { "vars": "all", "args": "after-used", "ignoreRestSiblings": false
+        }
+      ],
+      "max-len": ["error", {
+        "code": 250
+      }],
+      "max-lines-per-function": ["error", 50]
+  }
 }
